8b4ddffe3402119263375b3d79e366bcb02c69ca
Gabe Hoffman <gabehoffman@me.com>
2018-09-20 09:06:57 -0400

    Bootstrapped Single View Project

---
+++ FlowCoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlowCoordinatorPattern
+//
+//  Created by Gabe Hoffman on 9/20/18.
+//  Copyright © 2018 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
